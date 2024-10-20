50a74fb818b1938ce5660d634f61bc5325064995
Ren Tamdang <tamdangren@gmail.com>
2024-10-20 12:55:41 +0800

    Player object edits
    Includes creation, movement, and Player made as prefab

---
+++ Mapua Admin Building Tour/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2f;
+    private Rigidbody2D body;
+    private Vector2 movementDirection;
+
+    void Start() {
+        // Upon start, get access to the rigidbody that this object is attached to
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        // Check for keyboard input
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate() {
+        body.velocity = movementDirection * movementSpeed;
+    }
+}
