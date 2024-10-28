6debe2877f50346f26a803fabc2efe6088ce5810
Ren Tamdang <tamdangren@gmail.com>
2024-10-28 17:24:44 +0800

    SEARCH BAR FOR ALL FLOORS

--- Mapua Admin Building Tour/Assets/Scripts/SearchManager.cs
+++ Mapua Admin Building Tour/Assets/Scripts/SearchManager.cs
@@ -31,13 +31,64 @@ public class SearchManager : MonoBehaviour
             switch (searchTxt)
             {
                 case "canteen":
-                    setTarget(-2.996069f,-4.115806f);
+                    setTarget(-2.996069f, -4.115806f);
                     break;
                 case "book store":
-                    setTarget(3.543062f,-3.7354f);
+                    setTarget(3.543062f, -3.7354f);
                     break;
                 case "admin":
-                    setTarget(3.364418f,0.3528565f);
+                    setTarget(3.364418f, 0.3528565f);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (sceneIndex == 2)
+        {
+            switch (searchTxt)
+            {
+                case "oss":
+                    setTarget(-4.22604f,-3.827846f);
+                    break;
+                case "wellness room":
+                    setTarget(-3.009216f,-4.568482f);
+                    break;
+                case "mmcm offices":
+                    setTarget(4.54986f,-3.228403f);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (sceneIndex == 3) 
+        {
+            switch (searchTxt)
+            {
+                case "shs library":
+                    setTarget(-2.912398f,0.4198242f);
+                    break;
+                case "labs":
+                    setTarget(-1.556679f,-1.907591f);
+                    break;
+                case "college library":
+                    setTarget(3.69711f,0.3545743f);
+                    break;
+                case "chapel":
+                    setTarget(1.261744f,-1.670723f);
+                    break;
+                case "venue":
+                    setTarget(3.273902f,-1.716728f);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (sceneIndex == 4) 
+        {
+            switch (searchTxt)
+            {
+                case "auditoriums":
+                    setTarget(-3.462987f,-2.786807f);
                     break;
                 default:
                     break;
@@ -46,9 +97,11 @@ public class SearchManager : MonoBehaviour
         return found;
     }
 
-    void setTarget(float x,float y) {
-        target = new Vector2(x,y);
+    void setTarget(float x, float y)
+    {
+        target = new Vector2(x, y);
         found = true;
+        
     }
 
     void FixedUpdate()
