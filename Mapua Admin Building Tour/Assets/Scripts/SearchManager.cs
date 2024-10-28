31d9f64356a9c067db6306bab6f4cf821dbd6b89
Ren Tamdang <tamdangren@gmail.com>
2024-10-28 16:27:16 +0800

    Search bar now fucking WOOOORKS on the first floor

--- Mapua Admin Building Tour/Assets/Scripts/SearchManager.cs
+++ Mapua Admin Building Tour/Assets/Scripts/SearchManager.cs
@@ -1,29 +1,61 @@
 using UnityEngine;
 using TMPro;
-using System;
-
+using UnityEngine.SceneManagement;
 public class SearchManager : MonoBehaviour
 {
     public GameObject searchBar;
     public GameObject player;
+    private Rigidbody2D body;
+    private Scene scene;
+    private int sceneIndex;
     Vector2 target;
     float step;
+    bool found;
 
     // Start is called before the first frame update
     void Start()
     {
+        scene = SceneManager.GetActiveScene();
+        sceneIndex = scene.buildIndex;
         step = 2f * Time.deltaTime;
+        body = player.GetComponent<Rigidbody2D>();
+        target = new Vector2(0f, 0f);
     }
 
-    // Update is called once per frame  
-    public void Search()
+    public bool Search()
     {
-        string searchTxt = searchBar.GetComponent<TMP_InputField>().text;
-        if (searchTxt.Equals("Canteen"))
+        string searchTxt = searchBar.GetComponent<TMP_InputField>().text.ToLower();
+        found = false;
+        if (sceneIndex == 1)
         {
-            Debug.Log("yippee");
-            target = new Vector2(-2.996069f, -4.115806f);
-            player.transform.position = Vector2.MoveTowards(player.transform.position, target, step);
+            switch (searchTxt)
+            {
+                case "canteen":
+                    setTarget(-2.996069f,-4.115806f);
+                    break;
+                case "book store":
+                    setTarget(3.543062f,-3.7354f);
+                    break;
+                case "admin":
+                    setTarget(3.364418f,0.3528565f);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return found;
+    }
+
+    void setTarget(float x,float y) {
+        target = new Vector2(x,y);
+        found = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (Search() == true)
+        {
+            body.transform.position = Vector2.MoveTowards(body.transform.position, target, step);
         }
     }
 }
