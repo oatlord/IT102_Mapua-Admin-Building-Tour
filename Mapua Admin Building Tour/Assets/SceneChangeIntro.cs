029c6ac471c8bf36deb75226cb51339cbda5b417
magigugu <maguie.gooe@gmail.com>
2024-11-08 12:31:08 +0800

    scene change atempt and more colliders bweehh

---
+++ Mapua Admin Building Tour/Assets/SceneChangeIntro.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChangeIntro : MonoBehaviour
+{
+    public float changeTime;
+    public string sceneName;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        changeTime -= Time.deltaTime;
+        if(changeTime <= 0){
+            SceneManager.LoadScene(sceneName);
+        }
+        
+        
+    }
+}
